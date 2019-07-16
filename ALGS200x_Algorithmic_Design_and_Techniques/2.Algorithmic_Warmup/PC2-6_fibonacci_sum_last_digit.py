edc21b81f833f480663006a2f57c76d3f8a46607
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2019-07-16 09:45:44 +0200

    PC2-6 completed

--- ALGS200x_Algorithmic_Design_and_Techniques/2.Algorithmic_Warmup/PC2-6_fibonacci_sum_last_digit.py
+++ ALGS200x_Algorithmic_Design_and_Techniques/2.Algorithmic_Warmup/PC2-6_fibonacci_sum_last_digit.py
@@ -18,7 +18,6 @@ def fibonacci_sum_naive(n):
     return sum % 10
 """
 
-<<<<<<< HEAD
 # Knowing that Pisano Period
 # of modulo 10 is 60:
 
@@ -35,39 +34,6 @@ def fibonacci_sum(n):
         fib_arr.append((fib_arr[-1] + fib_arr[-2]) % 10)
 
     return (fib_arr[-1] - 1) % 10
-=======
-# Knowing that the Pisano Period
-# when modulo is 10,
-# is equal to 60:
-
-def fibonacci_sum(n):
-
-    def fib_mod_ten(n, m = 10):
-        if n < 2: return n
-
-        results = [1, 1]
-        for _ in range(n - 2):
-            results.append((results[-1] + results[-2]) % m)
-
-        return results[-1]
-
-
-    def pisano_period(m):
-        """
-        This returns the Pisano period
-        relative to the divisor 'm'.
-        """
-        previous = 1
-        current = 1
-
-        result = 1
-        while not (previous == 0 and current == 1):
-            previous, current = current, (previous + current) % m
-            result += 1
-
-        return result
-
-    return fib_mod_ten(n%pisano_period(m = 10))
 
 
 
