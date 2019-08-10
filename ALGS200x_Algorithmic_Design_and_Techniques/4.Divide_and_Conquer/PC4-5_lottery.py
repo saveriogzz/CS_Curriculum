778bfa9365b6f696b260f95ba36d3f03d9753ce0
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2019-08-10 17:52:37 +0200

    PC4-5 completed

--- ALGS200x_Algorithmic_Design_and_Techniques/4.Divide_and_Conquer/PC4-5_lottery.py
+++ ALGS200x_Algorithmic_Design_and_Techniques/4.Divide_and_Conquer/PC4-5_lottery.py
@@ -16,8 +16,34 @@ def lottery_naive(intervals, bets):
 
     return count
 
+def lottery_sort(segments, bets):
 
+    segments = [(segments[2*i], segments[2*i + 1])
+                        for i in range(len(segments) // 2)]
 
+    s_open = [i[0] for i in segments]
+    s_close = [i[1] for i in segments]
+
+    s_open.sort()
+    s_close.sort()
+
+    bets = sorted([bet for bet in bets])
+
+    res = []
+    count = 0
+    o = 0
+    c = 0
+    size = len(segments)
+    for bet in bets:
+        while o < size and s_open[o] <= bet:
+            o += 1
+            count += 1
+        while c < size and s_close[c] < bet:
+            c += 1
+            count -= 1
+        res.append(count)
+
+    return sum(res)
 
 
 if __name__ == "__main__":
@@ -26,4 +52,4 @@ if __name__ == "__main__":
     p = data[1]
     intervals = data[2:(s+1)*2]
     bets = data[(s+1)*2:]
-    print(lottery_naive(intervals, bets))
+    print(lottery_sort(intervals, bets))
