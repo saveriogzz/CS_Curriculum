ecf5704fe563c4bb84cf21f662c33dbe1eb95bd9
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2019-09-16 21:36:35 +0200

    PC4-6 completed

--- ALGS200x_Algorithmic_Design_and_Techniques/4.Divide_and_Conquer/PC4-6_closest_point.py
+++ ALGS200x_Algorithmic_Design_and_Techniques/4.Divide_and_Conquer/PC4-6_closest_point.py
@@ -1,19 +1,62 @@
 import sys
-from math import sqrt
+import math
 
 def euclidean(a, b):
-    return sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
+    """compute euclidean distance between two points
+    (tuples with x and y as elements)"""
 
-def closest_pair(points):
+    return math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2)
+
+def sort_X(points):
+    """sort list of points according to x coordinate"""
 
     n = len(points)//2
     A = [(points[2*i], points[2*i + 1]) for i in range(n)]
-
     Ax = sorted(A, key = lambda x: x[0])
+    return Ax
+
+def sort_Y(points):
+    """sort list of points according to x coordinate"""
+
+    n = len(points)//2
+    A = [(points[2*i], points[2*i + 1]) for i in range(n)]
     Ay = sorted(A, key = lambda x: x[1])
+    return Ay
 
-    return 
+def filter_strip(points, m, d):
+    """filter a list o points based on their x coordinate
+    to be included between (m - d) and (m - d)"""
 
+    strip = []
+    for point in points:
+        if abs(point[0] - m) < d:
+            strip.append(point)
+    return strip
+
+def find_min_dist(Ax):
+    """find the minimum distance between a set of points"""
+
+    n = len(Ax)//2
+    lhs, rhs = Ax[:n], Ax[n:]
+
+    if len(Ax) <= 3: return euclidean(Ax[0], Ax[1])
+    else:
+        leftSet_minDist = find_min_dist( lhs )
+        rightSet_minDist = find_min_dist( rhs )
+
+    d = min(leftSet_minDist, rightSet_minDist)
+    median = (lhs[-1][0] + rhs[0][0]) / 2
+
+    strip = sort_Y(filter_strip(Ax, median, d))
+
+    # need to check just the 6
+    for i in range(len(strip)):
+        js = min(len(strip), i + 7)
+        for j in range(i + 1, js):
+            ds = euclidean(strip[i][0], strip[j][0])
+            if ds < d: d = ds
+
+    return round(d)
 
 
 if __name__ == "__main__":
@@ -22,4 +65,5 @@ if __name__ == "__main__":
     n = data[0]
     points = data[1:]
 
-    print(closest_pair(points))
+    sorted_points = sort_X(points)
+    print(find_min_dist(sorted_points))
