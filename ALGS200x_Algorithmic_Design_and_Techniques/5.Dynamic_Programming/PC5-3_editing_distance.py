5ec5309144075cb3aea79eebacf73a99c3693af7
Saverio Guzzo <saverio.g.guzzo@gmail.com>
2019-12-31 18:04:24 +0100

    PC5-3 solved

--- ALGS200x_Algorithmic_Design_and_Techniques/5.Dynamic_Programming/PC5-3_editing_distance.py
+++ ALGS200x_Algorithmic_Design_and_Techniques/5.Dynamic_Programming/PC5-3_editing_distance.py
@@ -6,8 +6,8 @@ def editing_distance(A, B):
     for k in range(1, len(A)+1):
         matrix.append([k] + [0]*len(B))
 
-    for i in range(1, len(matrix)):
-        for j in range(1, len(matrix[0])):
+    for i in range(1, len(A)+1):
+        for j in range(1, len(B)+1):
             if A[i-1] == B[j-1]:
                 matrix[i][j] = min(matrix[i][j-1]+1, matrix[i-1][j]+1, matrix[i-1][j-1])
             else:
